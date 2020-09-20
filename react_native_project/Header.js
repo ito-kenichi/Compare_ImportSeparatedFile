1b558b3b4d9e23b6de2ac573dcf6a20b63337248
伊藤健一 <itoukenichi@MacBook-Pro-Developer.local>
2020-09-20 18:59:15 +0900

    update react native

--- react_native_project/Header.js
+++ react_native_project/Header.js
@@ -1,31 +1,18 @@
 import React from "react";
-
 import { View, Text, StyleSheet } from "react-native";
 
-const Header = props => {
-  return (
-    <View style={styles.containerHeader}>
-      <View style={styles.textContainer}>
-        <Text style={styles.textWhite}>Holi</Text>
-        <Text style={styles.textWhite}>1,004 tweets</Text>
-      </View>
-      <View style={styles.tabContainer}>
-        <View>
-          <Text>Tweets</Text>
-        </View>
-        <View>
-          <Text>Tweets & Replies</Text>
-        </View>
-        <View>
-          <Text>Media</Text>
-        </View>
-        <View>
-          <Text>Likes</Text>
+export default class Header extends React.Component{
+  render() {
+    return (
+      <View style={styles.containerHeader}>
+        {alert(this.props.propsname)}
+        <View style={styles.textContainer}>
+    <Text style={styles.textWhite}>{this.props.propsname}</Text>
         </View>
       </View>
-    </View>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   containerHeader: {
@@ -52,4 +39,3 @@ const styles = StyleSheet.create({
     height: 40
   }
 });
-export default Header;
